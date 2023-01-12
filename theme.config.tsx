8a89ad11dfeeb34eab7ac86ac352b937557fe14f
yumin-jung <jungym887@naver.com>
2023-01-12 23:42:47 +0900

    Init: 블로그 시작

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Blog</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/yumin-jung/blog',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  docsRepositoryBase: 'https://github.com/yumin-jung/blog'
 }
 
 export default config
