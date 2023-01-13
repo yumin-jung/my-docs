5fcc30fd88be8c0ab5d6f03f4415527284147c92
yumin-jung <jungym887@naver.com>
2023-01-14 00:17:40 +0900

    Update: 초기 페이지 및 폴더 생성

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Blog</span>,
+  logo: <span>기록</span>,
   project: {
     link: 'https://github.com/yumin-jung/blog',
   },
